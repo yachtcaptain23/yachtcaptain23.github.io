9640fb3b01c541ccf1cd5719d0155cb0083a793d
Albert <amwang217@gmail.com>
2025-11-08 21:58:43 -0800

    I hope this works

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import { getNotionPosts } from '../lib/notion';
 import Link from 'next/link';
 import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
+  const allNotionData = getNotionPosts();
   return {
     props: {
       allPostsData,
+      allNotionData
     },
   };
 }
@@ -59,7 +62,7 @@ export default function Home({ allPostsData}) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Writings</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.filter(item => item.post_type == 'writing' && item.visible == 'true').map(({ id, date, title }) => (
+          {allNotionData.filter(item => item.post_type == 'writing' && item.visible == 'true').map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
             <Link href={`/posts/${id}`}>{title}</Link>
             <br />
